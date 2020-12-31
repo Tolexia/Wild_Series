0cf9a4121063b334f3743b03a195ab553c7aaa64
Axel <axelcroizepro@gmail.com>
2020-12-31 15:37:30 +0100

    added feature : FileUpload for categories. Categories now displayed as cards

--- src/Form/CategoryType.php
+++ src/Form/CategoryType.php
@@ -7,6 +7,9 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Form\CallbackTransformer;
 
 
 class CategoryType extends AbstractType
@@ -14,7 +17,30 @@ class CategoryType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name', TextType::class)
+            ->add('name', TextType::class, ['label' => "Nom de la catégorie"])
+            ->add('img', FileType::class, [
+                'label' => 'Image bannière (jpg, jpeg, png, webp)',
+                'required' => false,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '2m',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                            'image/webp'
+                        ],
+                        'mimeTypesMessage' => 'Seuls les fichiers jpg, jpeg, png et webp sont acceptés',
+                    ])
+                ],
+            ]);
+            $builder->get('img')->addModelTransformer(new CallBackTransformer(
+                function($img) {
+                    return null;
+                },
+                function($img) {
+                    return $img;
+                }
+            ));
         ;
     }
 
